52820c60a2e84e96a1f9bb95619683cd8f6b8803
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2013-05-26 16:47:45 -0700

    Add support for handoff.

--- src/riak_pubsub_subscribe_vnode.erl
+++ src/riak_pubsub_subscribe_vnode.erl
@@ -8,6 +8,7 @@
 -behaviour(riak_core_vnode).
 
 -include_lib("riak_pubsub.hrl").
+-include_lib("riak_core/include/riak_core_vnode.hrl").
 
 -export([start_vnode/1,
          init/1,
@@ -24,28 +25,36 @@
          handle_coverage/4,
          handle_exit/3]).
 
--record(state, {partition}).
+-record(state, {partition, channels}).
 
 %% API
 start_vnode(I) ->
     riak_core_vnode_master:get_vnode_pid(I, ?MODULE).
 
 init([Partition]) ->
-    {ok, #state{partition=Partition}}.
+    {ok, #state{partition=Partition, channels=dict:new()}}.
 
 %% @doc Respond to a subscription; launch a child process,
 %%      and register it with gproc under a given channel name.
-handle_command({subscribe, Channel, Pid}, _Sender, State) ->
+handle_command({subscribe, Channel, Pid},
+               _Sender,
+               #state{channels=Channels0}=State) ->
     lager:warning("Received subscribe for ~p and ~p.\n",
                   [Channel, Pid]),
-    riak_pubsub_subscription_sup:start_child(Channel, Pid),
-    {reply, ok, State};
+
+    case subscribe(Channels0, Channel, Pid) of
+        {error, Error} ->
+            {reply, {error, Error}, State};
+        {ok, Channels} ->
+            {reply, ok, State#state{channels=Channels}}
+    end;
 handle_command(Message, _Sender, State) ->
     ?PRINT({unhandled_command, Message}),
     {noreply, State}.
 
-handle_handoff_command(_Message, _Sender, State) ->
-    {noreply, State}.
+handle_handoff_command(?FOLD_REQ{foldfun=Fun, acc0=Acc0}, _Sender, State) ->
+    Acc = dict:fold(Fun, Acc0, State#state.channels),
+    {reply, Acc, State}.
 
 handoff_starting(_TargetNode, State) ->
     {true, State}.
@@ -56,14 +65,24 @@ handoff_cancelled(State) ->
 handoff_finished(_TargetNode, State) ->
     {ok, State}.
 
-handle_handoff_data(_Data, State) ->
-    {reply, ok, State}.
+handle_handoff_data(Data, #state{channels=Channels0}=State) ->
+    {Channel, Pids} = binary_to_term(Data),
 
-encode_handoff_item(_ObjectName, _ObjectValue) ->
-    <<>>.
+    case subscribe(Channels0, Channel, Pids) of
+        {error, Error} ->
+            {reply, {error, Error}, State};
+        {ok, Channels} ->
+            {reply, ok, State#state{channels=Channels}}
+    end.
 
-is_empty(State) ->
-    {true, State}.
+encode_handoff_item(Channel, Pids) ->
+    term_to_binary({Channel, Pids}).
+
+is_empty(#state{channels=Channels}=State) ->
+    case dict:size(Channels) of
+        0 -> {true, State};
+        _ -> {false, State}
+    end.
 
 delete(State) ->
     {ok, State}.
@@ -76,3 +95,23 @@ handle_exit(_Pid, _Reason, State) ->
 
 terminate(_Reason, _State) ->
     ok.
+
+%% @doc Subscribe to a channel with a given pid, and return an
+%%      updated dict of channel to pid mappings.
+-spec subscribe(dict(), term(), pid() | list(pid())) -> dict().
+subscribe(Channels0, Channel, Pid) when is_pid(Pid) ->
+    case riak_pubsub_subscription_sup:start_child(Channel, Pid) of
+        {error, Error} ->
+            {error, Error};
+        _ ->
+            Channels = try
+                            dict:append_list(Channel, [Pid], Channels0)
+                       catch
+                            _:_ ->
+                                dict:store(Channel, [Pid], Channels0)
+                       end,
+            {ok, Channels}
+    end;
+subscribe(Channels0, Channel, Pids0) when is_list(Pids0) ->
+    lists:foldl(fun(Pid, Channels) ->
+                subscribe(Channels, Channel, Pid) end, Channels0, Pids0).

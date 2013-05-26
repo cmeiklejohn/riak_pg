c5f8409fe0ee8cc34cbad08f83a0b5f2ec1e8eeb
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2013-05-26 11:50:12 -0700

    Add copyright.

--- include/riak_pubsub.hrl
+++ include/riak_pubsub.hrl
@@ -1,1 +1,5 @@
+%% @author Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
+%% @copyright 2013 Christopher Meiklejohn.
+%% @doc Library definitions.
+
 -define(PRINT(Var), io:format("DEBUG: ~p:~p - ~p~n~n ~p~n~n", [?MODULE, ?LINE, ??Var, Var])).
